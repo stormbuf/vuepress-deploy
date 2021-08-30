fd158418133b41c0c5e824145a21094170bb63c0
stormbuf <aazzss55@163.com>
2021-08-30 12:07:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,7 @@ if [ "$TARGET_LINK" ]; then
 fi
 
 git clone https://${USERNAME}:${PASSWORD}github.com/stormbuf/blog.git
-echo $(ls -al)
+echo "$(ls -al)"
 git checkout -b master origin/main
 
 # Build vuepress project
